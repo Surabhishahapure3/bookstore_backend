368ea4b5b75dae86292d78b737395b54f3840c67
Surabhishahapure3 <surabhishahapure3@gmail.com>
2024-10-16 01:10:14 +0530

    [surabhi]added:validators file

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -2,16 +2,34 @@ import Joi from '@hapi/joi';
 import { Request, Response, NextFunction } from 'express';
 
 class UserValidator {
-  public newUser = (req: Request, res: Response, next: NextFunction): void => {
+  public register = (req:Request, res:Response,next:NextFunction): void=>{
     const schema = Joi.object({
-      name: Joi.string().min(4).required()
+      firstname: Joi.string().min(4).required(),
+      lastname : Joi.string().min(4).required(),
+      email : Joi.string().email().min(3).required(),
+      password : Joi.string().min(6).required(),
+      role: Joi.string().valid('user', 'admin').optional(),
+      confirmpassword : Joi.string().valid(Joi.ref("password")).required()
     });
     const { error } = schema.validate(req.body);
     if (error) {
       next(error);
     }
     next();
-  };
+  }
+
+  public emailValidate = (req:Request,res:Response,next:NextFunction):void =>{
+    const schema = Joi.object({
+      email: Joi.string().required(),
+      password : Joi.string().min(6).required(),
+      role: Joi.string().valid('user', 'admin').optional()
+    });
+    const { error } = schema.validate(req.body);
+    if (error) {
+      next(error);
+    }
+    next();
+  }
 }
 
 export default UserValidator;
